2e91356256c3c7a5d80d8eb973dd6ceda029ddc1
Vassil Kurktchiev <kurkweb@gmail.com>
2017-01-12 16:41:25 -0500

    Minor
    Minor Files changes

---
+++ js/myJS/jQuery.js
@@ -0,0 +1,11 @@
+$(document).ready(main);
+
+function main() {
+
+  $('.paper').hide();
+  $('.paper_design').hide();
+	"use strict"
+ 
+  $('.paper_design').fadeIn(1660);
+  $('.paper').fadeIn(1660);
+}
